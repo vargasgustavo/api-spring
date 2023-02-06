19d4260dd71602e0acfc25d6a45d14c3aaa69d25
vargasgustavo <gustavo.neumann.v@gmail.com>
2023-02-06 12:56:49 -0300

    Localizar pelo código

--- src/main/java/com/apostila/demo/Controller.java
+++ src/main/java/com/apostila/demo/Controller.java
@@ -32,6 +32,14 @@ public class Controller {
         return produto;
     }
 
+    @CrossOrigin(origins = "*", methods = RequestMethod.GET)
+    @DeleteMapping("/produto_localizar/{codigo}")
+    public List<Produto> Loacalizar(@PathVariable(value = "codigo") int p) {
+        ProdutoDAO dao = new ProdutoDAO();
+        System.out.println(dao.localizar(p));
+        return "Produto localizado!";
+    }
+
     @CrossOrigin(origins = "*", methods = RequestMethod.DELETE)
     @DeleteMapping("/produto_excluir/{codigo}")
     public String Excluir(@PathVariable(value = "codigo") int p) {
